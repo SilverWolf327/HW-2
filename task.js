6cc137475ece4f8287cf3e31cfdf579224b41d62
Aleksandr Guzeev <aleksandr.guzeeff@yandex.ru>
2022-11-14 16:55:05 +0300

    Initial commit

---
+++ task.js
@@ -0,0 +1,59 @@
+// Задание 1
+function getArrayParams(arr) {
+  let min, max, sum, avg;
+
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
+
+  return { min: min, max: max, avg: avg };
+}
+
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+
+  return sum;
+}
+
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+
+  for (i = 0; i < arrOfArr.length; i++) {
+      arrResultElement = func(arrOfArr[i]);
+      if (arrResultElement > max) max = arrResultElement;
+  }
+
+  return max;
+}
+
+// Задание 3
+function worker2(arr) {
+  let min = arr[0];
+  let max = arr[0];
+
+   for (i = 0; i < arr.length; i++) {
+     if (arr[i] < min) min = arr[i];
+     if (arr[i] > max) max = arr[i];
+   }
+
+   let delta = max - min;
+   
+   return delta;
+}
